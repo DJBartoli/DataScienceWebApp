3feefbc12b5e1a14bb22c3bc8e2f93333620cfca
Anton-2303 <79974045+Anton-2303@users.noreply.github.com>
2024-03-20 16:06:43 +0100

    2nd page update

--- pages/categoriesInteractions.py
+++ pages/categoriesInteractions.py
@@ -5,6 +5,7 @@ import json
 import pandas as pd
 import dash
 from dash import dcc, html, callback
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -14,16 +15,23 @@ from dash.dependencies import Input, Output
 import json
 import dash_bootstrap_components as dbc
 
+import numpy as np
+import random
+
 dash.register_page(__name__,  name='Categories Interactions')
 
+# Load CSVs
+
 catdf = pd.read_csv('data/duration/categoryInteractions.csv')
+
+catdf2 = pd.read_csv('data/categoryData/Categories_Formatted.csv')
 
 # Barchart
 
 fig = go.Figure(data=[
     go.Bar( name='Average Likes', x=catdf['Category'], y=catdf['Average Likes']),
     go.Bar( name='Avegare Comments', x=catdf['Category'], y=catdf['Average Comments'], marker_color='#dd2b2b')
-])
+],)
 
 fig.update_layout(title_text='Average viewer interactions in different categories', barmode='stack',
             plot_bgcolor='#e7e7e7',
@@ -54,6 +62,7 @@ layout = html.Div(
                 id='category-bar', figure=fig),
                 width={'size': 7, 'offset': 1},
                 style={'padding': '5px', 'background-color': '#d1d1d1', 'border-radius': '10px', 'box-shadow': '0px 2px 5px #949494'},
+                
                 ),
             dbc.Col(html.H5('''
                         This barchart shows the average likes and comments in selected categories.
@@ -65,6 +74,85 @@ layout = html.Div(
                         
                     '''))
             ]),
+
+            dbc.Row([
+                dbc.Col(html.Hr(style={'margin': '20px 0', 'border': 'none', 'border-top': '1px solid #ccc'}),
+                width={'size':10, 'offset':1}
+                        )
+            ],
+            style={'height':'50px'},
+            ),
+
+            dbc.Col(
+                html.H2('Additional Data on Categories', style={'color': '#dd2b2b'}),
+                width={'size': 5, 'offset': 1},
+                style={'height':'70px'}
+            ),
+
+            dbc.Row(
+                dbc.Col(dcc.Dropdown(
+                id='category-drop',
+                options=[{'label': 'Average Views', 'value': 'Views'},
+                        {'label': 'Average Video Length', 'value': 'Length'},
+                        ],
+                        value='Views',
+                        clearable=False
+                        ),
+
+                        style={'color': '#262626'},
+                        width={'size': 2, 'offset': 1} ),
+
+            ),
+
+            dbc.Row(dbc.Row(html.H5(),style={'height':'20px'})),
+
+            dbc.Row([
+                dbc.Col(dcc.Graph(
+                id='category-bar-2',),
+                width={'size': 7, 'offset': 1},
+                style={'padding': '5px', 'background-color': '#d1d1d1', 'border-radius': '10px', 'box-shadow': '0px 2px 5px #949494'},
+                
+                ),
+                dbc.Col(html.H5('''
+                        TODO
+                        '''))
+            ]),
+
+            
     ]),
     
 )
+
+# Callback
+
+@callback(
+    Output('category-bar-2', 'figure'),
+    [Input('category-drop', 'value')]
+)
+
+# Callback Function
+
+def update_category_bar(selected_value) :
+
+    vdf = catdf2.groupby('Title')['Video_Views'].mean().reset_index()
+
+    sdf = catdf2.groupby('Title')['Seconds'].mean().reset_index()
+
+    if selected_value == 'Views' :
+
+        trace = go.Bar(x=vdf['Title'], y=vdf['Video_Views'], marker_color='#dd2b2b', name='Barchart1')
+
+        layout = go.Layout(title=f'Barchart for {selected_value}',xaxis_title='Video Category',yaxis_title='Average Video Views', plot_bgcolor='#e7e7e7', paper_bgcolor='#d1d1d1')
+
+        return {'data': [trace], 'layout': layout}
+    
+    elif selected_value == 'Length' :
+
+        trace2 = go.Bar(x=sdf['Title'], y=sdf['Seconds'],  name='Barchart2')
+
+        layout = go.Layout(title=f'Barchart for {selected_value}',xaxis_title='Video Category',yaxis_title='Average Video Length in Seconds', plot_bgcolor='#e7e7e7', paper_bgcolor='#d1d1d1')
+
+        return {'data': [trace2], 'layout': layout}
+    
+
+
